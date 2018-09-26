a8f5c9c2bab12a2d81d915b064776ca1d0d02ffc
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-09-26 10:17:45 -0700

    keyboard demo

--- Panel.py
+++ Panel.py
@@ -10,20 +10,45 @@ class CommandPannel:
     def __init__(self):
         self.root = tk.Tk()
 
-        self.fd=Tk.Button(root,text='Forwards',command=d1_Euler_wrapper)
-        self.bk=Tk.Button(root,text='Back',command=d1_Euler_wrapper)
-        self.rt=Tk.Button(root,text='Right',command=d1_Euler_wrapper)
-        self.lt=Tk.Button(root,text='Left',command=d1_Euler_wrapper)
+        self.fd=tk.Button(self.root,text='Forwards',command=self.ha)
+        self.bk=tk.Button(self.root,text='Back',command=self.ha)
+        self.rt=tk.Button(self.root,text='Right',command=self.ha)
+        self.lt=tk.Button(self.root,text='Left',command=self.ha)
+
+
+        self.fd.pack()
+        self.bk.pack()
+        self.rt.pack()
+        self.lt.pack()
 
 
 
 
-def publish():
-    pub.send()
-    root.after(100, publish)
-
-root.after(100, publish)
+        self.root.bind('<Left>', self.leftKey)
 
 
+        self.root.mainloop()
 
-root.mainloop()
+    @staticmethod
+    def ha():
+        print "ha"
+
+    @staticmethod
+    def leftKey(veent):
+        print "leftKey"
+
+        
+
+
+    def publish(self):
+        self.pub.send()
+        self.root.after(100, publish)
+
+    # self.root.after(100, publish)
+
+
+if __name__ == "__main__":
+    a = CommandPannel()
+
+
+
