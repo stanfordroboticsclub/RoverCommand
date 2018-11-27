ba0f85bf3701bab8145416b38b6692e77057c6ad
Michal <mikh3x4@gmail.com>
2018-11-27 03:36:01 +0000

    added yes to install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-sudo apt-get install libsdl-ttf2.0-0
-sudo pip install ds4drv
-sudo pip3 install pygame
+sudo apt-get install -y libsdl-ttf2.0-0
+yes | sudo pip install ds4drv
+yes | sudo pip3 install pygame
