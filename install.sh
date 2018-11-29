ad705c91d8cd19bd71bd8636a18053eeb8722b75
Michal Adamkiewicz <mikh3x4@gmail.com>
2018-11-29 03:35:13 -0800

    install.sh installs service files now

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+FOLDER=$(dirname $(realpath "$0"))
+cd $FOLDER
+
 sudo apt-get install -y libsdl-ttf2.0-0
 yes | sudo pip install ds4drv
 yes | sudo pip3 install pygame
+
+for file in *.service; do
+    [ -f "$file" ] || break
+    sudo ln -s $FOLDER/$file /lib/systemd/system/
+done
