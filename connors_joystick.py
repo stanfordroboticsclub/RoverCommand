fbbc5ad6502e2eb45d22a02008a05c88a0519557
Connor Cremers <connor.cremers@gmail.com>
2019-05-07 16:49:20 -0700

    Update connors_joystick.py

--- connors_joystick.py
+++ connors_joystick.py
@@ -25,40 +25,35 @@ print(JoyName)
 JoyAx = pygame.joystick.Joystick(0).get_numaxes()
 print("Number of axis:")
 print(JoyAx)
-mode = "arm"
-#mode_file = open("/tmp/robot_joystick_mode.txt","r")
+
+mode_file = open("/tmp/robot_joystick_mode.txt","r")
 
 # Prints the values for axis0
 while True:
-
-   # mode_file.seek(0)
-   # mode = mode_file.read()
+    print("running")
+    mode_file.seek(0)
+    mode = mode_file.read()
+    
     pygame.event.pump()
 
-    PS = pygame.joystick.Joystick(0).get_button(10)
-    hat = pygame.joystick.Joystick(0).get_hat(0)
-    if(PS == 1) and (hat[0] == 1 or hat[1] == 1):
-    	mode = "safe"
-    elif (PS == 1) and (hat[0] == -1):
-    	mode = "arm"
-    elif (PS ==1) and (hat[1] == -1):
-    	mode = "drive" 
-    
-
     if mode.startswith('drive'):
-        forward = (pygame.joystick.Joystick(0).get_axis(5))
+        forward_left = (pygame.joystick.Joystick(0).get_axis(1))
+        forward_right = (pygame.joystick.Joystick(0).get_axis(5))
         twist = (pygame.joystick.Joystick(0).get_axis(2))
-        on = (pygame.joystick.Joystick(0).get_button(5))
 
+        on_right = (pygame.joystick.Joystick(0).get_button(5))
+        on_left = (pygame.joystick.Joystick(0).get_button(4))
 
-        if on:
-            drive_pub.send({'f':-150*forward,'t':-80*twist})
+        if on_left:
+            drive_pub.send({'f':-150*forward_left,'t':-80*twist})
+        elif on_right:
+            drive_pub.send({'f':-150*forward_right,'t':-80*twist})
         else:
             drive_pub.send({'f':0,'t':0})
 
     if mode.startswith('arm'):
-        r_forward  = -(pygame.joystick.Joystick(0).get_axis(4))
-        r_side = (pygame.joystick.Joystick(0).get_axis(3))
+        r_forward  = -(pygame.joystick.Joystick(0).get_axis(5))
+        r_side = (pygame.joystick.Joystick(0).get_axis(2))
 
         l_forward  = -(pygame.joystick.Joystick(0).get_axis(1))
         l_side = (pygame.joystick.Joystick(0).get_axis(0))
@@ -66,14 +61,15 @@ while True:
         r_shoulder  = (pygame.joystick.Joystick(0).get_button(5))
         l_shoulder  = (pygame.joystick.Joystick(0).get_button(4))
 
-        r_trigger  = (pygame.joystick.Joystick(0).get_axis(5))
-        l_trigger = (pygame.joystick.Joystick(0).get_axis(2))
+        r_trigger  = (pygame.joystick.Joystick(0).get_axis(4))
+        l_trigger = (pygame.joystick.Joystick(0).get_axis(3))
 
-        square  = (pygame.joystick.Joystick(0).get_button(3))
-        cross  = (pygame.joystick.Joystick(0).get_button(0))
-        circle  = (pygame.joystick.Joystick(0).get_button(1))
-        triangle  = (pygame.joystick.Joystick(0).get_button(2))
+        square  = (pygame.joystick.Joystick(0).get_button(0))
+        cross  = (pygame.joystick.Joystick(0).get_button(1))
+        circle  = (pygame.joystick.Joystick(0).get_button(2))
+        triangle  = (pygame.joystick.Joystick(0).get_button(3))
 
+        PS  = (pygame.joystick.Joystick(0).get_button(12)) 
 
         # print("button")
         # for i in range(14):
@@ -82,18 +78,32 @@ while True:
         # for i in range(12):
         #     print(i, pygame.joystick.Joystick(0).get_axis(i))
 
+        hat = pygame.joystick.Joystick(0).get_hat(0)
+
         reset = (PS == 1) and (triangle == 1)
 
-        target_vel = {"x": l_side,
+        if(PS == 0):
+              target_vel = {"x": l_side,
                       "y": l_forward,
-                      "z": (r_trigger-l_trigger)/2,
+                      "z": (r_trigger - l_trigger)/2,
                       "yaw": r_side,
                       "pitch": r_forward,
                       "roll": (r_shoulder - l_shoulder),
                       "grip": cross - square,
                       "hat": hat,
-                      "reset": reset}
-
+                      "reset": reset,
+                      "trueXYZ": circle}
+        else:
+              target_vel = {"x": 0,
+                      "y": 0,
+                      "z": 0,
+                      "yaw": 0,
+                      "pitch": 0,
+                      "roll": 0,
+                      "grip": 0,
+                      "hat": (0,0),
+                      "reset": reset,
+                      "trueXYZ": 0}
         print(target_vel)
         arm_pub.send(target_vel)
     else:
