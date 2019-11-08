3cba2e55b20b619cef0832862a20329f0b8049b7
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-11-08 14:08:39 -0800

    cleanup mac joystick

--- mac_joystick.py
+++ mac_joystick.py
@@ -9,7 +9,7 @@ arm_pub = Publisher(8410)
 pygame.display.init()
 pygame.joystick.init()
 
-# wait untill joystick is connected
+# wait until joystick is connected
 while 1:
     try:
         pygame.joystick.Joystick(0).init()
@@ -26,28 +26,17 @@ JoyAx = pygame.joystick.Joystick(0).get_numaxes()
 print("Number of axis:")
 print(JoyAx)
 
-#mode_file = open("/tmp/robot_joystick_mode.txt","r")
-
-# Prints the values for axis0
 while True:
-
-#    mode_file.seek(0)
-#    mode = mode_file.read()
-    mode = 'drive'
-
     pygame.event.pump()
 
-    if mode.startswith('drive'):
-        forward = (pygame.joystick.Joystick(0).get_axis(3))
-        twist = (pygame.joystick.Joystick(0).get_axis(2))
-        on = (pygame.joystick.Joystick(0).get_button(5))
+    forward = (pygame.joystick.Joystick(0).get_axis(3))
+    twist = (pygame.joystick.Joystick(0).get_axis(2))
+    on = (pygame.joystick.Joystick(0).get_button(5))
 
-        if on:
-            print({'f':-150*forward,'t':-80*twist})
-            drive_pub.send({'f':-150*forward,'t':-80*twist})
-        else:
-            drive_pub.send({'f':0,'t':0})
+    if on:
+        print({'f':-150*forward,'t':-80*twist})
+        drive_pub.send({'f':-150*forward,'t':-80*twist})
     else:
-        pass
+        drive_pub.send({'f':0,'t':0})
 
     pygame.time.wait(100)
